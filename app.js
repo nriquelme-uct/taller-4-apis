661d9c97d2775e99d6d833f658ca409d768f0c87
unknown <nriquelme2022@alu.uct.cl>
2025-06-01 16:33:35 -0400

    Añadir recursos iniciales.

---
+++ app.js
@@ -0,0 +1,30 @@
+
+// Definir constantes de Node.js.
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// Metodos:
+
+// GET
+app.get();
+
+// POST
+app.post();
+
+// PUT
+app.put();
+
+// PATCH
+app.patch();
+
+// HEAD
+app.head();
+
+// DELETE
+app.delete();
+
+// OPTIONS
+app.options();
